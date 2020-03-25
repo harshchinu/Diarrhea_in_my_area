cda54a1aa0674fc3f74df16b111648a8155d6924
Harsh Maheshwari <mharsh301@gmail.com>
2020-03-25 13:15:44 +0530

    done with firebase authetication in both activity goverment login and hospital login

---
+++ app/src/main/java/com/example/diarrheainmyarea/HospialMain.java
@@ -0,0 +1,14 @@
+package com.example.diarrheainmyarea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HospialMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospial_main);
+    }
+}
--- app/src/main/java/com/example/diarrheainmyarea/MainActivity.java
+++ app/src/main/java/com/example/diarrheainmyarea/MainActivity.java
@@ -2,16 +2,36 @@ package com.example.diarrheainmyarea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button hospital,government;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this,NavigationActivity.class));
+            hospital=(Button)findViewById(R.id.hospital_login);
+            government=(Button)findViewById(R.id.government_login);
+
+            hospital.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this,Hospital_login.class));
+                }
+            });
+
+            government.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this,goverment_login.class));
+                }
+            });
     }
 }
