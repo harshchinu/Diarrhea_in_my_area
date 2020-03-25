b1a9e5af27cdcf342df7437d64f9488efe6b5fc7
tailorsmit <sjtailor27@gmail.com>
2020-03-25 07:31:13 +0530

    added separate activity for bottom navigation. added 2 fragments for the navigation realtime fragment and statistical fragment

---
+++ app/src/main/java/com/example/diarrheainmyarea/NavigationActivity.java
@@ -0,0 +1,27 @@
+package com.example.diarrheainmyarea;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class NavigationActivity extends AppCompatActivity {
+    BottomNavigationView bnv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+        bnv=findViewById(R.id.navigationbar);
+        AppBarConfiguration abc=new AppBarConfiguration.Builder(
+                R.id.realtimeFragment,R.id.statisticalFragment
+        ).build();
+        NavController navc= Navigation.findNavController(this,R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this,navc,abc);
+        NavigationUI.setupWithNavController(bnv,navc);
+    }
+}
