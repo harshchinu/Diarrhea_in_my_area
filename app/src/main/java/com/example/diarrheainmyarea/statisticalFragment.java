5f36fe8c165434220183b06b03821f9bf4462a5a
Mihir Mehta <mmihir1998@gmail.com>
2020-03-26 09:24:21 +0530

    statistical fragment graph added
    Signed-off-by: Mihir Mehta <mmihir1998@gmail.com>

--- app/src/main/java/com/example/diarrheainmyarea/statisticalFragment.java
+++ app/src/main/java/com/example/diarrheainmyarea/statisticalFragment.java
@@ -2,11 +2,33 @@ package com.example.diarrheainmyarea;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.ColumnChartView;
 
 
 /**
@@ -15,6 +37,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class statisticalFragment extends Fragment {
+
+    private DatabaseReference dbDates,mDatabase;
+    private ColumnChartView chart;
+    private ColumnChartData data;
+    private Spinner diseaseSpinner;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -59,6 +86,113 @@ public class statisticalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistical, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_statistical, container, false);
+        dbDates = FirebaseDatabase.getInstance().getReference().child("Date");
+        chart = (ColumnChartView) rootView.findViewById(R.id.columnChartCorona);
+        diseaseSpinner = rootView.findViewById(R.id.graphDiseaseSpinner);
+        diseaseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                generateData();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        spinnerFill();
+
+
+
+        return rootView;
+
+
     }
+
+    private void spinnerFill(){
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Diseases");
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                final List<String> titleList = new ArrayList<String>();
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+
+                    String name = dataSnapshot1.getValue(String.class);
+                    titleList.add(name);
+                }
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, titleList);
+                arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                diseaseSpinner.setAdapter(arrayAdapter);
+                generateData();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+
+    private void generateData(){
+
+        final String stDisease = diseaseSpinner.getSelectedItem().toString();
+
+        final List<String> keys = new ArrayList<>();
+
+        final List<AxisValue> valuesOfX = new ArrayList<AxisValue>();
+        dbDates.addListenerForSingleValueEvent(new ValueEventListener() {
+            int i =0;
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Column> columns = new ArrayList<>();
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                    List<SubcolumnValue> values= new ArrayList<SubcolumnValue>();
+                    AxisValue temp = new AxisValue(i++);
+                    temp.setLabel(ds.getKey());
+                    valuesOfX.add(temp);
+                    if(ds.child(stDisease).exists()){
+                        values.add(new SubcolumnValue(ds.child(stDisease).getValue(Integer.class),ChartUtils.pickColor()));
+                    }
+
+                    else {
+                        values.add(new SubcolumnValue(0,ChartUtils.pickColor()));
+                    }
+                    Column column = new Column(values);
+                    column.setHasLabels(true);
+                    columns.add(column);
+                   Log.e("THE DATA",ds.child("Corona").getValue().toString());
+                }
+                setValues(columns,valuesOfX);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+//
+//        values.add(new SubcolumnValue(20, ChartUtils.pickColor()));
+//        values.add(new SubcolumnValue(50, ChartUtils.pickColor()));
+//        values.add(new SubcolumnValue(20, ChartUtils.pickColor()));
+
+    }
+
+    private void setValues(List<Column> columns,List<AxisValue> valuesOfX ){
+
+        data = new ColumnChartData(columns);
+
+        Axis axisX = new Axis();
+        Axis axisY = new Axis().setHasLines(true);
+        axisX.setName("Dates");
+        axisY.setName("Number of cases");
+        axisX.setValues(valuesOfX);
+        data.setAxisXBottom(axisX);
+        data.setAxisYLeft(axisY);
+
+        chart.setColumnChartData(data);
+    }
+
 }
